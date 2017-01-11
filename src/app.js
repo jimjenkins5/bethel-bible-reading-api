1270e095af7297ad21df16ff5ff723b04484b726
Jim Jenkins <jimjenkins5@gmail.com>
2017-01-11 16:56:52 -0500

    cors support, scaffold routes started

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
@@ -13,7 +14,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.get('/ping', (req, res) => {
+   res.json({ ping: 'pong' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
